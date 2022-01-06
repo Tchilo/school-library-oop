678c0f74196d8b52d6e8e3c1f017d88474540065
Tchilo <tchiloross@gmail.com>
2022-01-06 08:53:32 +0200

    Add corrector file

--- person.rb
+++ person.rb
@@ -1,3 +1,5 @@
+require_relative('./corrector')
+
 class Person
   attr_accessor :name, :age
   attr_reader :id
@@ -6,9 +8,14 @@ class Person
     @id = Random.rand(1..1000)
     @name = name
     @age = age
+    @corrector = Corrector.new
     @parent_permission = parent_permission
   end
 
+  def validate_name
+    @name = @corrector.correct_name(@name)
+  end
+  
   def can_use_services?
     of_age? || @parent_permission
   end
