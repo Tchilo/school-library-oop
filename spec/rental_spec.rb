4bbabd21741e439dceee23691ead4a2a471627df
Tchilo <tchiloross@gmail.com>
2022-01-13 12:01:05 +0200

    Add test for rental_spec

--- spec/rental_spec.rb
+++ spec/rental_spec.rb
@@ -4,4 +4,12 @@ require_relative '../book'
 
 describe Rental do
   context 'When providing a book and a person' do
+    person = Person.new(id: nil, age:20, name: 'Luke', parent_permission: true)
+    book = Book.new('Habits', 'James')
+    rented = Rental.new('01-02-2022', book, person)
+
+    it 'Should create rental for person' do
+      expect(person.rentals).to include(rented)
+    end
+  end
 end
