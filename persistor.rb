0e477bcb3962a61485f8e54d265b81e51ff62e2e
yahya <crazy20046@gmail.com>
2022-01-13 14:22:20 +0200

    Update persistor.rb

--- persistor.rb
+++ persistor.rb
@@ -1,4 +1,3 @@
-# rubocop:disable ...
 
 require 'json'
 
@@ -76,8 +75,8 @@ class Persistor
   end
 
   def persist(people:, rentals:, books:)
-    File.open('people.json', 'w') { |f| f.write JSON.generate(people) } unless people.empty?
-    File.open('books.json', 'w') { |f| f.write JSON.generate(books) } unless books.empty?
-    File.open('rentals.json', 'w') { |f| f.write JSON.generate(rentals) } unless rentals.empty?
+    File.open('people.json', 'w') { |f| f.write JSON.generate(people) } unless people.empty?# rubocop:todo Lint/UselessAssignment
+    File.open('books.json', 'w') { |f| f.write JSON.generate(books) } unless books.empty?# rubocop:todo Lint/UselessAssignment
+    File.open('rentals.json', 'w') { |f| f.write JSON.generate(rentals) } unless rentals.empty?# rubocop:todo Lint/UselessAssignment
   end
 end
