cf931a77bad544f162bfbff9ae1b90edba2de995
yahya <crazy20046@gmail.com>
2022-01-13 12:33:22 +0200

    Update persistor.rb

--- persistor.rb
+++ persistor.rb
@@ -74,8 +74,8 @@ class Persistor
   end
 
   def persist(people:, rentals:, books:)
-    File.open('people.json', 'w') { |f| f.write JSON.generate(people) } unless people.empty?
-    File.open('books.json', 'w') { |f| f.write JSON.generate(books) } unless books.empty?
-    File.open('rentals.json', 'w') { |f| f.write JSON.generate(rentals) } unless rentals.empty?
+    File.open('people.json', 'w') { |file| file.write JSON.generate(people) } unless people.empty?
+    File.open('books.json', 'w') { |file| file.write JSON.generate(books) } unless books.empty?
+    File.open('rentals.json', 'w') { |file| file.write JSON.generate(rentals) } unless rentals.empty?
   end
 end
