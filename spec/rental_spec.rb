1d5d5fb8e0a57775c70896655db54be42a8d6c21
Tchilo <tchiloross@gmail.com>
2022-01-13 12:43:16 +0200

    Add test for classroom

--- spec/rental_spec.rb
+++ spec/rental_spec.rb
@@ -4,7 +4,7 @@ require_relative '../book'
 
 describe Rental do
   context 'When providing a book and a person' do
-    person = Person.new(id: nil, age:20, name: 'Luke', parent_permission: true)
+    person = Person.new(age:20, name: 'Luke', parent_permission: true)
     book = Book.new('Habits', 'James')
     rented = Rental.new('01-02-2022', book, person)
 
